c12fb1b1f7197a9ff132423151f51ef0f2ae690f
Ben Atanesyan <benatanesyan2@gmail.com>
2022-01-22 21:47:13 +0400

    code cleanup and some properties validation addition

--- BookOrder.Services/Entities/Book.cs
+++ BookOrder.Services/Entities/Book.cs
@@ -9,17 +9,21 @@ namespace BookOrder.Entities
     {
         [Key]
         public int Id { get; set; }
+
         [Required]
         public string Name { get; set; }
+
         [Required]
         public int GenreId { get; set; }
+
         [Required]
         public int PageCounts { get; set; }
-        
+
         public string Author { get; set; }
 
         [Required]
         public bool IsAvailable { get; set; }
+
         [Required]
         public int InStockCount { get; set; }
     }
--- BookOrder.Services/Entities/Member.cs
+++ BookOrder.Services/Entities/Member.cs
@@ -9,17 +9,22 @@ namespace BookOrder.Entities
     {
         [Key]
         public int Id { get; set; }
-        public Guid AccountId { get; set; }
+
         [Required]
         public string Name { get; set; }
+
         [Required]
         public string Surname { get; set; }
+
         [Required]
         public DateTime Birthdate { get; set; }
+
         [Required]
         public Genders Gender { get; set; }
+
         [Required]
         public string Address { get; set; }
+
         [Required]
         public string Phone { get; set; }
 
